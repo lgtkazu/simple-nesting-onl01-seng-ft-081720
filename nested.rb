d80b3d43a423d4f409dc0dbe724c8c4f960a35f1
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-31 01:21:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,7 @@ end
 end
 
 def dennis_ritchies_language
-@@ -55,6 +58,9 @@ def dennis_ritchies_language
+
           :languages => ["C"]
         }
      }
@@ -29,7 +29,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-@@ -80,6 +86,10 @@ def adding_matz
+
         }
      }
 
@@ -40,7 +40,7 @@ programmer_hash.merge(:yukihiro_matsumoto => {
 
 end
 
-@@ -102,7 +112,8 @@ def changing_alan
+
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
 
@@ -50,7 +50,7 @@ end
 end
 
 def adding_to_dennis
-@@ -121,7 +132,8 @@ def adding_to_dennis
+
           :languages => ["C"]
         }
      }
