c290fa1d85c51ae09cd80e5b30abd6afe54f5cd2
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-31 01:23:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,17 @@
 def hopper
 	programmer_hash = 
  		{
-
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
         }
      }
 
